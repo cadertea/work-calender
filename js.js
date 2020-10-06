ef42d9a1d5394b568c21a873cd27737954bfaa4b
Cadertea <cadertea@ncsu.edu>
2020-10-06 11:52:28 -0400

    added some comment lines,also added classes to timeblocks which are added based on if statements, and fixed issue where functions wasnt working by ading it in the document ready brackets

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-
+    
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
     //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         //get nearby values.
-        console.log(this);
-        var text = $(this).siblings(".description").val();
+        // console.log(this);
+        
         var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
 
         //set items in local storage.
         localStorage.setItem(time, text);
@@ -23,19 +24,33 @@ $(document).ready(function () {
     $("#16 .description").val(localStorage.getItem("16"));
     $("#17 .description").val(localStorage.getItem("17"));
 
-
     function timetracker() {
-        //gets what time it is now in hours
+        //gets what time it is now in hours from your computer
         var currentime = moment().hour();
 
-        // turns the string 9 into a integer value
+        
         $(".time-block").each(function () {
             var idtime = parseInt($(this).attr("id"));
-            console.log( idtime, currentime)
+            // console.log( idtime, currentime)
 
-          
+            //check if we've moved past this time
+            if (idtime < currentime) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+               
+            }
+            else if (idtime === currentime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
         })
     }
-  
-
+    timetracker();
 })
