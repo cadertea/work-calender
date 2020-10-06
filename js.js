dc7c5f80779ce762227bcc4dadd6973a14ded0f4
Cadertea <cadertea@ncsu.edu>
2020-10-06 09:50:31 -0400

    added local storage commands and text box now save

---
+++ js.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    //Assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#9  .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+    
